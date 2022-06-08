600b873c9b18a21dc3ab3410cdf01fbe4a766103
herisusantoarismann <heri.s.arisman@gmail.com>
2022-06-08 13:02:41 +0700

    Fix and add logo

--- pages/Login.jsx
+++ pages/Login.jsx
@@ -1,7 +1,32 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Button, Drawer } from "antd";
 
 export const Login = () => {
+  const [visible, setVisible] = useState(false);
+
+  const showDrawer = () => {
+    setVisible(true);
+  };
+
+  const onClose = () => {
+    setVisible(false);
+  };
+
   return (
-    <div>Login</div>
-  )
-}
+    <>
+      <Button type="primary" onClick={showDrawer}>
+        Open
+      </Button>
+      <Drawer
+        title="Basic Drawer"
+        placement="right"
+        onClose={onClose}
+        visible={visible}
+      >
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+      </Drawer>
+    </>
+  );
+};
